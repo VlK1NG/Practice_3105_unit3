2be8ec51fc281418aa218b4792cd815b89b0a9b8
_-V!K!NG-_ <gendalfthevvhite@gmail.com>
2022-05-31 19:35:22 +0300

    Overriding abstract methods for FolkDanceRobot Overriding default dance type for FolkDanceRobot

---
+++ src/com/absoft/unit3/FolkDanceRobot.java
@@ -0,0 +1,19 @@
+package com.absoft.unit3;
+
+public class FolkDanceRobot extends DancingRobot{
+
+    @Override
+    public String getDanceType1() {
+        return "Country road";
+    }
+
+    @Override
+    public String getDanceType2() {
+        return "Sweet home Alabama";
+    }
+
+    @Override
+    public String getDefaultDanceType() {
+        return "American pie";
+    }
+}
